814d29193d392e940249034d98b08fb39e64068d
arthurlanz <arthurlanznaster@gmail.com>
2026-03-24 14:47:52 -0300

    feat: adicionar modelo e API para veículos

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -2,3 +2,4 @@ from .user import UserRegistrationView, UserViewSet
 from .acessorio import AcessorioViewSet
 from .cor import CorViewSet
 from .modelo import ModeloViewSet
+from .veiculos import VeiculoViewSet
